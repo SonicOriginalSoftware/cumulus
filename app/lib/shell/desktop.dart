3729d10a855a916eea16cedd16454fb8c2f2a7ce
Nathan Blair <me@nathanblair.rocks>
2022-09-19 13:43:46 -0400

    wip

--- app/lib/shell/desktop.dart
+++ app/lib/shell/desktop.dart
@@ -1,5 +1,6 @@
 import 'package:code_repository/shell/bar/display.dart';
 import 'package:code_repository/shell/body.dart';
+import 'package:code_repository/shell/drawer/panel.dart';
 import 'package:flutter/material.dart';
 
 class Desktop extends StatelessWidget {
@@ -7,9 +8,15 @@ class Desktop extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: Bar(),
-      body: Body(),
+    return Scaffold(
+      appBar: const Bar(),
+      body: Flex(
+        direction: Axis.horizontal,
+        children: const [
+          AppPanel(),
+          Expanded(flex: 1, child: Body()),
+        ],
+      ),
     );
   }
 }
--- app/lib/shell/mobile.dart
+++ app/lib/shell/mobile.dart
@@ -1,16 +1,16 @@
 import 'package:code_repository/shell/bar/display.dart';
 import 'package:code_repository/shell/body.dart';
+import 'package:code_repository/shell/drawer/panel.dart';
 import 'package:flutter/material.dart';
 
 class Mobile extends StatelessWidget {
   const Mobile({super.key});
 
-  static const widthBreakpoint = 600;
-
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       appBar: Bar(),
+      drawer: AppPanel(),
       body: Body(),
     );
   }
