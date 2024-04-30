44d23491d4e0ddfe9d61e78c7c25bbb8a2fdce04
Nathan Blair <me@nathanblair.rocks>
2024-04-30 02:52:39 -0400

    add some more logging for database interaction

--- src/lib/server/validate_auth_tables.js
+++ src/lib/server/validate_auth_tables.js
@@ -3,9 +3,13 @@
  * @returns
  */
 export async function get_tables(db) {
-  const { success, results } = await db.prepare("PRAGMA table_list").all()
+  const timeout = new Promise((res) => setTimeout(() => res({ success: false, results: ["timed out"] }), 5000))
+  const statement = db.prepare("PRAGMA table_list").all()
+
+  const { success, results } = await Promise.race([timeout, statement])
+
   if (!success) {
-    return
+    return results
   }
 
   let tables = []
--- src/routes/api/heal/+server.js
+++ src/routes/api/heal/+server.js
@@ -6,19 +6,37 @@ import { error } from "@sveltejs/kit"
  * @param {import("../../$types.js").RequestEvent} event
  */
 export async function GET(event) {
+  console.debug("Checking auth database initialization...")
   // Eventually we'll put this behind an authenticated check
 
   /** @type {import("@cloudflare/workers-types").D1Database} */
   const db = event.platform?.env.auth
 
-  const tables = await get_tables(db)
-  if (tables === null) {
+  console.debug(db)
+
+  let tables
+  try {
+    tables = await get_tables(db)
+  } catch (err) {
+    error(500, err)
+  }
+  if (tables === undefined || tables === null || tables[0] === "timed out") {
+    error(500, "Could not get tables")
+  }
+
+  console.debug(tables)
+
+  if (tables?.length === 0) {
+    console.debug("Initializing auth database...")
     try {
       await up(db)
     } catch (err) {
       error(500, err)
     }
   }
+  console.debug("Auth database initialized")
+
+  // TODO Check table info for each expected table
 
   return Response.json({ status: "ok" })
 }
